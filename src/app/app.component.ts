867547bcecd353a800f52d6a197ba651de5dcdc7
fiotto <fiotto42@gmail.com>
2019-09-06 21:00:03 +0900

    STEP4 Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+interface Todos{
+  high: string[]
+  normal: string[]
+  low: string[]
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,4 +14,9 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title: string = 'Angular';
   isSelectdItem: boolean = Math.random() > 0.5;
+  todos: Todos = {
+    high: ['掃除', '洗濯', '炊事'],
+    normal: ['買い物', '草刈り', 'アイロン'],
+    low: ['窓拭き', '振り込み', '家計簿管理']
+  };
 }
