18eaafe16a6da226dbbbcf52c7688fc52b3621ae
fiotto <fiotto42@gmail.com>
2019-09-18 21:51:44 +0900

    STEP8 Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -33,4 +33,22 @@ export class AppComponent {
   onClickTodo(todo): void {
     this.selectedItem = todo;
   }
+
+  onClickPriority(signum): void {
+    const keys: string[] = Object.keys(this.todos);
+    for(let idx in keys){
+      const key: string = keys[idx];
+
+      if(this.todos[key].includes(this.selectedItem)){
+        this.todos[key] = this.todos[key].filter((a: string) => a !== this.selectedItem);
+
+        const destIndex: number = (signum > 0) ? Number(idx)-1 : Number(idx)+1;
+        const dest: string = keys[destIndex];
+        this.todos[dest].push(this.selectedItem);
+
+        this.selectedItem = null;
+        break;
+      }
+    }
+  }
 }
