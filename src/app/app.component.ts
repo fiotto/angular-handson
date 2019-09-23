f291fa9842d5e410740865a68ea80feeb3801639
fiotto <fiotto42@gmail.com>
2019-09-23 23:12:31 +0900

    STEP9 Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,9 +14,9 @@ interface Todos{
 export class AppComponent {
   title: string = 'Angular';
   todos: Todos = {
-    high: ['掃除', '洗濯', '炊事'],
-    normal: ['買い物', '草刈り', 'アイロン'],
-    low: ['窓拭き', '振り込み', '家計簿管理']
+    high: [],
+    normal: [],
+    low: []
   };
   inputItem: string = '';
   selectedItem: string | null = null;
